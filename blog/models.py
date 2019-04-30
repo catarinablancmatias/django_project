4d626812cad0dfca715598a16b923f3ba59a6341
catarinablancmatias <49726694+catarinablancmatias@users.noreply.github.com>
2019-04-30 16:01:30 +0100

    Update models.py

--- blog/models.py
+++ blog/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.conf import settings
 from django.urls import reverse
 from PIL import Image
+
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
@@ -14,20 +16,18 @@ class Post(models.Model):
     def __str__(self):
     	return self.title
 
-    #def save(self, *args, **kwargs):
-     #       super().save(*args, **kwargs)
-#
- #           img = Image.open(self.image.path)
-#
- #           if img.height > 150 or img.width >150:
-  #              output_size = (150, 150)
-   #             img.thumbnail(output_size)
-    #            img.save(self.image.path)
+    def save(self, *args, **kwargs):
+            super().save(*args, **kwargs)
+
+            img = Image.open(self.image.path)
+
+            if img.height > 150 or img.width >150:
+                output_size = (150, 150)
+                img.thumbnail(output_size)
+                img.save(self.image.path)
 
     def snippet(self):
-    	return self.content[:50] + '...'
+    	return self.content[:13] + '...'
 
     def get_absolute_url(self):
     	return reverse('post-detail', kwargs={'pk': self.pk})
-
-    
